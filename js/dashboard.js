c628294d047fb89573befa70ffeb460fcfe64825
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-06-12 13:51:05 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,48 @@
+const usuario =
+localStorage.getItem("usuarioRH");
 
+const rol =
+localStorage.getItem("rolRH");
+
+if(!usuario){
+
+    window.location =
+    "login.html";
+
+}
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+document.getElementById(
+"usuarioInfo"
+).innerHTML =
+
+`
+Bienvenido: <b>${usuario}</b><br>
+Rol: ${rol}
+`;
+
+});
+
+function abrirModulo(url){
+
+    window.location = url;
+
+}
+
+function cerrarSesion(){
+
+    localStorage.removeItem(
+    "usuarioRH"
+    );
+
+    localStorage.removeItem(
+    "rolRH"
+    );
+
+    window.location =
+    "login.html";
+
+}
