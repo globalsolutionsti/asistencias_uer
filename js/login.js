f9012ee9f167af78d5f6625f268bdf2c9008a8dd
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-06-12 13:08:11 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,57 @@
+const API_URL =
+"https://TU_WEBAPP/exec";
 
+function login(){
+
+const usuario =
+document.getElementById("usuario").value;
+
+const password =
+document.getElementById("password").value;
+
+fetch(API_URL,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"text/plain"
+},
+
+body:JSON.stringify({
+
+accion:"loginRH",
+usuario:usuario,
+password:password
+
+})
+
+})
+
+.then(r=>r.json())
+
+.then(data=>{
+
+if(data.success){
+
+localStorage.setItem(
+"usuarioRH",
+data.nombre
+);
+
+localStorage.setItem(
+"rolRH",
+data.rol
+);
+
+window.location=
+"dashboard.html";
+
+}else{
+
+alert(data.message);
+
+}
+
+});
+
+}
