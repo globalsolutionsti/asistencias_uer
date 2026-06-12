c9950d1579dbe983cf682eb24c8d6b431fcf4c1c
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-06-12 13:30:51 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://TU_WEBAPP/exec";
+"https://script.google.com/macros/s/AKfycbxmySBZcwvBjz98Qk_Swts1RX2VaU9B_wZrFj2ptvefWohp0cGba-eq8rrj9NcUt1SWlg/exec";
 
 function login(){
 
