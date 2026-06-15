fd80cf1051c47faa18312aca13e0583fdab4946b
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-06-15 12:58:49 -0600

    Update incidencias.js

--- js/incidencias.js
+++ js/incidencias.js
@@ -1,7 +1,89 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbykJX05FrT2RNICQ10Ja6q0baU2LroCrePuvSklMirz2y4dohKMuQ7IDbmM9OFnmYmpsQ/exec";
+
 function consultarIncidencias(){
 
-    alert(
-      "Motor de incidencias listo para conectar"
-    );
+fetch(API_URL,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"text/plain"
+},
+
+body:JSON.stringify({
+
+accion:
+"consultarIncidencias"
+
+})
+
+})
+
+.then(r=>r.json())
+
+.then(data=>{
+
+mostrarTabla(data);
+
+})
+
+.catch(error=>{
+
+console.error(error);
+
+alert(
+"Error al consultar incidencias"
+);
+
+});
+
+}
+
+function mostrarTabla(datos){
+
+let html = `
+
+<table>
+
+<tr>
+
+<th>Fecha</th>
+<th>Empleado</th>
+<th>Nombre</th>
+<th>UER</th>
+<th>Incidencia</th>
+<th>Detalle</th>
+
+</tr>
+
+`;
+
+datos.forEach(row=>{
+
+html += `
+
+<tr>
+
+<td>${row.fecha}</td>
+<td>${row.empleado}</td>
+<td>${row.nombre}</td>
+<td>${row.uer}</td>
+<td>${row.incidencia}</td>
+<td>${row.detalle}</td>
+
+</tr>
+
+`;
+
+});
+
+html += "</table>";
+
+document
+.getElementById(
+"resultado"
+)
+.innerHTML = html;
 
 }
